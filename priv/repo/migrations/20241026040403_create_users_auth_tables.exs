e0f1f00c59499358f41223b32f53678b97064dd6
ariaandika <andikaakun17@gmail.com>
2024-10-26 12:20:50 +0700

    add more user fields

--- priv/repo/migrations/20241026040403_create_users_auth_tables.exs
+++ priv/repo/migrations/20241026040403_create_users_auth_tables.exs
@@ -5,14 +5,19 @@ defmodule World.Repo.Migrations.CreateUsersAuthTables do
     execute "CREATE EXTENSION IF NOT EXISTS citext", ""
 
     create table(:users) do
-      add :email, :citext, null: false
+      add :name, :string, null: false
+      add :role, :string, null: false
+      add :phone, :string, null: false
+      # add :email, :citext, null: false
       add :hashed_password, :string, null: false
       add :confirmed_at, :utc_datetime
 
       timestamps(type: :utc_datetime)
     end
 
-    create unique_index(:users, [:email])
+    create unique_index(:users, [:name])
+    create unique_index(:users, [:phone])
+    # create unique_index(:users, [:email])
 
     create table(:users_tokens) do
       add :user_id, references(:users, on_delete: :delete_all), null: false
